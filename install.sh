d5f92dfa8ddbc3dc92262a2cc72898242aa46ff8
oyvindhaugen <95085344+oyvindhaugen@users.noreply.github.com>
2023-05-22 12:04:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+mkdir /mnt/root
 curl -L https://raw.githubusercontent.com/oyvindhaugen/arch-vm-test/main/install_newshell.sh -o /mnt/root/part2.sh
 chmod +x /mnt/root/part2.sh
 timedatectl set-ntp true # sets the system clock to sync with network
@@ -36,5 +37,5 @@ mount /dev/sda1 /mnt/boot/efi # mount the EFI partition to the EFI directory
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf # enables parallel downloads
 pacstrap /mnt base base-devel linux linux-firmware # installs the base components of Linux
 genfstab -U /mnt >> /mnt/etc/fstab # generates the fstab file with UUIDs
-arch-chroot /mnt /mnt/root/part2.sh # enter the filesystem in the /mnt directory
+arch-chroot /mnt ./root/part2.sh # enter the filesystem in the /mnt directory
 reboot # reboots the system
