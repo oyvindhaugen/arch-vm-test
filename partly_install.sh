985488db3f678d39ffb88f559a9683cf0eaaf7d7
oyvindhaugen <95085344+oyvindhaugen@users.noreply.github.com>
2023-05-22 11:25:19 +0200

    Update partly_install.sh

--- partly_install.sh
+++ partly_install.sh
@@ -6,5 +6,5 @@ timedatectl set-ntp true # sets the system clock to sync with network
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf # enables parallel downloads
 pacstrap /mnt base base-devel linux linux-firmware # installs the base components of Linux
 genfstab -U /mnt >> /mnt/etc/fstab # generates the fstab file with UUIDs
-arch-chroot /mnt /root/part2.sh # enter the filesystem in the /mnt directory
+arch-chroot /mnt ./root/part2.sh # enter the filesystem in the /mnt directory
 #look at /etc/hostname, hosts and /boot/grub/grub.cfg
